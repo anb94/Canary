87b1c54dbef798d9e7fdd32b587634dc4e4568f3
anb94 <anbennett@pm.me>
2021-08-18 14:53:54 +0800

    autobuild

--- canary_upgrade.sh
+++ canary_upgrade.sh
@@ -1,17 +1,3 @@
-#!/bin/bash
-
-# Define environment variables
-## Define SHARE Root directory:
-build_dir=$HOME/test
-
-wget https://raw.githubusercontent.com/anb94/gwas_pancan/master/singularity_images/custom_singularity_images/canary_v5.def -O "${build_dir}"/canary.def
-sudo singularity build --sandbox "${build_dir}"/canary_v5.sif "${build_dir}"/canary.def
-sudo singularity shell --writable "${build_dir}"/canary_v5.sif ~/canary_upgrade.sh
-
-
-
-
-
 apt-get install -y default-jre openjdk-11-jre-headless openjdk-8-jre-headless default-jdk libcurl4 libcurl4-openssl-dev \
 libxml2-dev libssl-dev
 apt-key adv --keyserver keyserver.ubuntu.com --recv-keys E298A3A825C0D65DFD57CBB651716619E084DAB9
