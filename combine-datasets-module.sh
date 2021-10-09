4680b4412c93c8d5abd29c4c48956c652acf421a
anb94 <anbennett@pm.me>
2021-10-10 00:17:43 +0800

    combine

--- combine-datasets-module.sh
+++ combine-datasets-module.sh
@@ -3,8 +3,8 @@
 # manual input
 # Path of the *allchr.pdat input files - (should be the dir used in ${dose2plinkout}"/"${dataset}" from convert-mac-module.sh)
 
-dataset_1_dir=/home/anbennett2/scratch/datasets/processed_data/dbgap/WHI/WHIMS_dataset/
-dataset_2_dir=/home/anbennett2/scratch/datasets/processed_data/dbgap/WHI/GARNET_dataset/
+dataset_1_dir=/home/anbennett2/scratch/datasets/processed_data/dbgap/WHI/WHIMS_dataset
+dataset_2_dir=/home/anbennett2/scratch/datasets/processed_data/dbgap/WHI/GARNET_dataset
 datasets=()
 datasets=(${dataset_1_dir} ${dataset_2_dir})
 
@@ -55,7 +55,8 @@ echo "Generating file of shared snps between the datasets"
 
 #echo "$(comm -12 "${out_dir}"/*_snp-set.tsv > "${out_dir}"/"${output_name}"_sharedsnps.tsv
 #echo "Done generating file of shared snps"
-#echo "$(cut -d ' ' -f 1 "${out_dir}"/"${output_name}"_sharedsnps.tsv)" > "${out_dir}"/"${output_name}"_shared_alleles.tsv
+#echo "$(cut -d ' ' -f 1 "${out_dir}"/"${output_name}"_sharedsnps.tsv > "${out_dir}"/"${output_name}"_shared_alleles.tsv
+
 
 
 
@@ -88,7 +89,7 @@ echo "Completed Generating map files for datasets"
 # Collect low quality SNPs
 rm -rf "${out_dir}"/"${output_name}"_tempsnps.txt
 for i in "${datasets[@]}"; do
-  echo "Copying low quality snps for "${i}" to "${out_dir}""
+  echo "Copying low quality snps for "${i}" to "${out_dir}" "
 cat "${i}"/*_all_chr_lq_all_snps.txt >> "${out_dir}"/"${output_name}"_tempsnps.txt
 done
 echo "Finished combining low quality SNPs"
@@ -104,7 +105,7 @@ for i in "${datasets[@]}"; do
   plink2 --import-dosage "${i}"/*_allchr.pdat \
   --psam "${i}"/*_allchr.pfam \
   --map "${i}"/"${dtst}".map \
-  --extract "${i}"/"${dtst}"_sharedsnps.tsv \
+  --extract "${out_dir}"/"${output_name}"_sharedsnps.tsv \
   --exclude "${i}"/"${dtst}"_lq_all_snps.txt \
   --make-pgen \
   --out "${i}"/"${dtst}"_plink_temp1
